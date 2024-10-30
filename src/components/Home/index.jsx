d5ab67ce33bd6c8b0b1e81b685fcc072939884bf
Leandro Farias <leandrorf1606@gmail.com>
2024-10-30 19:27:45 -0300

    home arrumada

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -4,17 +4,17 @@ import { TopRate } from "../TopRate";
 
 export function Home() {
   return (
-    <div className="mb-3 ">
-      <h1 className="text-zinc-50 text-3xl px-8 py-10 ml-6 absolute z-10 font-bold">
+    <main>
+      <h1 className="text-zinc-50 text-3xl px-2 sm:px-8 py-10 ml-6 absolute z-10 font-bold">
         Cinevault
       </h1>
 
       <Carousel />
 
-      <div className="mt-5 space-y-10">
+      <div className="my-6 space-y-10">
         <TopRate />
         <Populares />
       </div>
-    </div>
+    </main>
   );
 }
